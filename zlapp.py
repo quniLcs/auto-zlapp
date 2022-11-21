18524d17b50b35145856af7fc4f1e2ae40951232
Yiqun Wang <87119318+quniLcs@users.noreply.github.com>
2022-11-21 18:57:47 +0800

    Add files via upload

--- zlapp.py
+++ zlapp.py
@@ -1,308 +1,260 @@
-import json
+# The original code is from https://github.com/FDUCSLG/pafd-automated
+# The following has been optimized
+
+# The log file can be sent to WeChat by PushPlus with reference to:
+# https://github.com/DullSword/GLaDOS-CheckIn
+# https://github.com/tyIceStream/GLaDOS_Checkin
+
+
 import time
 import os
-from json import loads as json_loads
-from os import path as os_path, getenv
-from sys import exit as sys_exit
-from getpass import getpass
+import logging
+
+import string
 import re
-import base64
+from json import loads
+
+from getpass import getpass
+
 import easyocr
 import io
 import numpy
-from PIL import Image
-from PIL import ImageEnhance
+from PIL import Image, ImageEnhance
 
-from requests import session, post, adapters
-adapters.DEFAULT_RETRIES = 5
+import requests
+
+
+def get_account(output = print):
+    # The first way to get account: from the environment variables
+    fudan_id = os.getenv("FUDAN_ID")
+    fudan_password = os.getenv("FUDAN_PASSWORD")
+    pushplus_token = os.getenv("PUSHPLUS_TOKEN")
+    if fudan_id is not None and fudan_password is not None and pushplus_token is not None:
+        output("Have obtained account information from the environment variables.")
+        return fudan_id, fudan_password, pushplus_token
+
+    # The second way to get account: from the file
+    if os.path.exists("account.txt"):
+        output("Read the account information from account.txt.")
+        with open("account.txt", "r") as account_file:
+            account_text = account_file.readlines()
+
+        fudan_id = account_text[0].split()[1]
+        fudan_password = account_text[1].split()[1]
+        pushplus_token = account_text[2].split()[1]
+        return fudan_id, fudan_password, pushplus_token
+
+    else:  # The third way to get account: input
+        output("account.txt not found.")
+        output("Please input the following account information:")
+
+        fudan_id = input("Fudan UIS ID: ")
+        fudan_password = getpass("Fudan UIS Password: ")
+        pushplus_token = input("PushPlus Token: ")
+
+        with open("account.txt", "w") as account_file:
+            account_file.write("FUDAN_ID: %s\nFUDAN_PASSWORD: %s\nPUSHPLUS_TOKEN: %s\n" %
+                               (fudan_id, fudan_password, pushplus_token))
+
+        output("account.txt saved.")
+        output("Please pay attention to the safety of the file.")
+        output("A hyperlink to desktop is recommended.")
+
+        return fudan_id, fudan_password, pushplus_token
+
+
+def read_captcha(image):
+    reader = easyocr.Reader(["en"])
+
+    image = Image.open(io.BytesIO(image)).convert("L")
+    image = ImageEnhance.Brightness(image).enhance(factor = 1.5)
+    image = numpy.array(image)
+
+    # without allow list, there may be unexpected spaces
+    allowlist = list(string.ascii_letters)
+    horizontal_list_agg, free_list_agg = reader.detect(image, optimal_num_chars=4)
+
+    result = reader.recognize(image,
+                              allowlist = allowlist,
+                              horizontal_list = horizontal_list_agg[0],
+                              free_list = free_list_agg[0],
+                              detail = 0)
+    return result[0]
+
 
 class Fudan:
-    """
-    建立与复旦服务器的会话，执行登录/登出操作
-    """
-    UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0"
+    # built a session with the server
+    def __init__(self, fudan_id, fudan_password, pushplus_token,
+                 login_url = "https://uis.fudan.edu.cn/authserver/login",
+                 log_file = '', output = print):
+        # the default value of login_url includes no service
 
-    # 初始化会话
-    def __init__(self,
-                 uid, psw,
-                 url_login='https://uis.fudan.edu.cn/authserver/login',
-                 url_code="https://zlapp.fudan.edu.cn/backend/default/code"):
-        """
-        初始化一个session，及登录信息
-        :param uid: 学号
-        :param psw: 密码
-        :param url_login: 登录页，默认服务为空
-        """
-        self.session = session()
-        self.session.keep_alive = False
-        self.session.headers['User-Agent'] = self.UA
-        self.url_login = url_login
-        self.url_code = url_code
+        self.fudan_id = fudan_id
+        self.fudan_password = fudan_password
+        self.pushplus_token = pushplus_token
+        self.login_url = login_url
+        self.log_file = log_file
+        self.output = output
 
-        self.uid = uid
-        self.psw = psw
+        self.session = requests.session()
 
-    def _page_init(self):
-        """
-        检查是否能打开登录页面
-        :return: 登录页page source
-        """
-        print("◉Initiating——", end='')
-        page_login = self.session.get(self.url_login)
+    def login_init(self):
+        # check if the login page is open
+        # if true, return the source code of the login page
 
-        print("return status code",
-              page_login.status_code)
+        self.output("Start to open the login page.")
+        login_page = self.session.get(self.login_url)
 
-        if page_login.status_code == 200:
-            print("◉Initiated——", end="")
-            return page_login.text
+        if login_page.status_code == 200:
+            self.output("Successfully open the login page.")
+            self.output('')
+            return login_page.text
         else:
-            print("◉Fail to open Login Page, Check your Internet connection\n")
+            self.output("The status code is %d." % login_page.status_code)
+            self.output("Fail to open the login page.")
+            self.output("Check your Internet connection.")
+            self.output('')
             self.close()
 
     def login(self):
-        """
-        执行登录
-        """
-        page_login = self._page_init()
+        login_page_text = self.login_init()
 
-        print("getting tokens")
-        data = {
-            "username": self.uid,
-            "password": self.psw,
-            "service": "https://zlapp.fudan.edu.cn/site/ncov/fudanDaily"
-        }
+        data = {"username": self.fudan_id, "password": self.fudan_password,
+                "service": "https://zlapp.fudan.edu.cn/site/ncov/fudanDaily"}
+        data.update(re.findall(r'<input type="hidden" name="([a-zA-Z0-9\-_]+)" value="([a-zA-Z0-9\-_]+)"/?>',
+                               login_page_text))
 
-        # 获取登录页上的令牌
-        result = re.findall(
-            '<input type="hidden" name="([a-zA-Z0-9\-_]+)" value="([a-zA-Z0-9\-_]+)"/?>', page_login)
-        # print(result)
-        # result 是一个列表，列表中的每一项是包含 name 和 value 的 tuple，例如
-        # [('lt', 'LT-6711210-Ia3WttcMvLBWNBygRNHdNzHzB49jlQ1602983174755-7xmC-cas'), ('dllt', 'userNamePasswordLogin'), ('execution', 'e1s1'), ('_eventId', 'submit'), ('rmShown', '1')]
-        data.update(
-            result
-        )
+        self.output("Start to log in.")
+        login_post = self.session.post(self.login_url, data = data, allow_redirects = False)
 
-        headers = {
-            "Host": "uis.fudan.edu.cn",
-            "Origin": "https://uis.fudan.edu.cn",
-            "Referer": self.url_login,
-            "User-Agent": self.UA
-        }
-
-        print("◉Login ing——", end="")
-        post = self.session.post(
-            self.url_login,
-            data=data,
-            headers=headers,
-            allow_redirects=False)
-
-        print("return status code", post.status_code)
-
-        if post.status_code == 302:
-            print("\n***********************"
-                  "\n◉登录成功"
-                  "\n***********************\n")
+        if login_post.status_code == 302:
+            self.output("Successfully log in.")
+            self.output('')
         else:
-            print("◉登录失败，请检查账号信息")
+            self.output("The status code is %d." % login_post.status_code)
+            self.output("Fail to log in.")
+            self.output("Please check the account information.")
+            self.output('')
             self.close()
 
     def logout(self):
-        """
-        执行登出
-        """
-        exit_url = 'https://uis.fudan.edu.cn/authserver/logout?service=/authserver/login'
-        expire = self.session.get(exit_url).headers.get('Set-Cookie')
-        # print(expire)
+        self.output("Start to log out.")
+        logout_url = "https://uis.fudan.edu.cn/authserver/logout?service=/authserver/login"
+        logout_page = self.session.get(logout_url)
 
-        if '01-Jan-1970' in expire:
-            print("◉登出完毕")
+        if "01-Jan-1970" in logout_page.headers.get("Set-Cookie"):
+            self.output("Successfully log out.")
+            self.output('')
         else:
-            print("◉登出异常")
+            self.output("Exception arises when logging out.")
+            self.output('')
 
-    def close(self, exit_code=0):
-        """
-        执行登出并关闭会话
-        """
+    def send(self):
+        with open(self.log_file, "r") as log_file:
+            content = log_file.read()
+
+        data = {"channel": "wechat", "token": self.pushplus_token,
+                "title": "Auto-Zlapp Log File", "content": content}
+
+        self.output('')
+        self.output("Start to send message.")
+        send_post = requests.post("https://www.pushplus.plus/send", data = data)
+        send_json = send_post.json()
+
+        if send_json["code"] == 200:
+            self.output("Successfully send message.")
+        else:
+            self.output("Fail to send Message")
+
+    def close(self):
+        # log out, close the session and send message
+
         self.logout()
+
         self.session.close()
-        print("◉关闭会话")
-        print("************************")
-        sys_exit(exit_code)
+        self.output("Session closed.")
+
+        if log_file:
+            self.send()
+
+        quit()
 
 
 class Zlapp(Fudan):
-    last_info = ''
-
     def check(self):
-        """
-        检查
-        """
-        print("◉检测是否已提交")
-        get_info = self.session.get(
-            'https://zlapp.fudan.edu.cn/ncov/wap/fudan/get-info')
-        last_info = get_info.json()
+        self.output("Start to check the Fudan zlapp.")
+        zlapp_page = self.session.get("https://zlapp.fudan.edu.cn/ncov/wap/fudan/get-info")
+        zlapp_all_info = zlapp_page.json()
 
-        print("◉上一次提交日期为:", last_info["d"]["info"]["date"])
-        # 地区从oldInfo中提取
-        last_area = last_info["d"]["oldInfo"]["area"]
-        print("◉上一次提交的地区是:", last_area)
-        if last_area == "其他国家":
-            pass
-        else:
-            position = last_info["d"]["info"]['geo_api_info']
-            position = json_loads(position)
+        zlapp_date = zlapp_all_info["d"]["info"]["date"]
+        self.output("Last submit date: %s." % zlapp_date)
 
-            print("◉上一次提交地址为:", position['formattedAddress'])
-            # print("◉上一次提交GPS为", position["position"])
-        # print(last_info)
-        
-        # 改为上海时区
-        os.environ['TZ'] = 'Asia/Shanghai'
-        time.tzset()
-        today = time.strftime("%Y%m%d", time.localtime())
-        print("◉今日日期为:", today)
-        if last_info["d"]["info"]["date"] == today:
-            print("\n*******今日已提交*******")
+        zlapp_last_area = zlapp_all_info["d"]["oldInfo"]["area"]
+        self.output("Last submit area: %s." % zlapp_last_area)
+
+        if zlapp_last_area != "其他国家":
+            zlapp_position = zlapp_all_info["d"]["info"]["geo_api_info"]
+            zlapp_position = loads(zlapp_position)
+            self.output("Last submit address: %s." % zlapp_position["formattedAddress"])
+
+        today_date = time.strftime("%Y%m%d", time.localtime())
+        self.output("Today's date: %s." % today_date)
+
+        if zlapp_date == today_date:
+            self.output("Submitted today.")
+            self.output('')
             self.close()
         else:
-            print("\n\n*******未提交*******")
-            self.last_info = last_info["d"]["oldInfo"]
-            
-    def read_captcha(self, img_byte):
-        img = Image.open(io.BytesIO(img_byte)).convert('L')
-        enh_bri = ImageEnhance.Brightness(img)
-        new_img = enh_bri.enhance(factor=1.5)
-        
-        image = numpy.array(new_img)
-        reader = easyocr.Reader(['en'])
-        horizontal_list, free_list = reader.detect(image, optimal_num_chars=4)
-        character = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        allow_list = list(character)
-        allow_list.extend(list(character.lower()))
-    
-        result = reader.recognize(image, 
-                                allowlist=allow_list,
-                                horizontal_list=horizontal_list[0],
-                                free_list=free_list[0],
-                                detail = 0)
-        return result[0]
-    
+            self.output("Not submitted today.")
+            self.output('')
+            self.zlapp_last_info = zlapp_all_info["d"]["oldInfo"]
 
-    def validate_code(self):
-        img = self.session.get(self.url_code).content
-        return self.read_captcha(img)
+    def submit(self):
+        self.output("Start to submit.")
 
-    def checkin(self):
-        """
-        提交
-        """
-        headers = {
-            "Host": "zlapp.fudan.edu.cn",
-            "Referer": "https://zlapp.fudan.edu.cn/site/ncov/fudanDaily?from=history",
-            "DNT": "1",
-            "TE": "Trailers",
-            "User-Agent": self.UA
-        }
+        while True:
+            self.output("Start to read the captcha.")
+            captcha_page = self.session.get("https://zlapp.fudan.edu.cn/backend/default/code")
+            code = read_captcha(captcha_page.content)
 
-        print("\n\n◉◉提交中")
-        
-        province = self.last_info["province"]
-        city = self.last_info["city"]
-        area = self.last_info["area"]
-        if area == "其他国家":
-            gwszdd = self.last_info["gwszdd"]
-        else:
-            geo_api_info = json_loads(self.last_info["geo_api_info"])
-            district = geo_api_info["addressComponent"].get("district", "")
-        
-        while(True):
-            print("◉正在识别验证码......")
-            code = self.validate_code()
-            print("◉验证码为:", code)
-            if area == "其他国家":
-                self.last_info.update(
-                    {
-                        "tw": "13",
-                        "province": province,
-                        "city": city,
-                        "area": area,
-                        "gwszdd": gwszdd,
-                        #"sfzx": "1",  # 是否在校
-                        #"fxyy": "",  # 返校原因
-                        "code": code,
-                    }
-                )
-            else:
-                self.last_info.update(
-                    {
-                        "tw": "13",
-                        "province": province,
-                        "city": city,
-                        "area": " ".join((province, city, district)),
-                        #"sfzx": "1",  # 是否在校
-                        #"fxyy": "",  # 返校原因
-                        "code": code,
-                    }
-                )
-            # print(self.last_info)
-            save = self.session.post(
-                'https://zlapp.fudan.edu.cn/ncov/wap/fudan/save',
-                data=self.last_info,
-                headers=headers,
-                allow_redirects=False)
+            self.output("The captcha is: %s." % code)
+            self.zlapp_last_info.update({"code": code})
 
-            save_msg = json_loads(save.text)["m"]
-            print(save_msg, '\n\n')
-            time.sleep(0.1)
-            if(json_loads(save.text)["e"] != 1):
+            submit_post = self.session.post("https://zlapp.fudan.edu.cn/ncov/wap/fudan/save",
+                                            data = self.zlapp_last_info, allow_redirects = False)
+
+            submit_result = loads(submit_post.text)
+            self.output("The result is: %s." % submit_result["m"])
+            if submit_result["e"] != 1:
                 break
 
-def get_account():
-    """
-    获取账号信息
-    """
-    uid = getenv("FUDAN_ID")
-    psw = getenv("FUDAN_PASSWORD")
-    if uid != None and psw != None:
-        print("从环境变量中获取了用户名和密码！")
-        return uid, psw
-    print("\n\n请仔细阅读以下日志！！\n请仔细阅读以下日志！！！！\n请仔细阅读以下日志！！！！！！\n\n")
-    if os_path.exists("account.txt"):
-        print("读取账号中……")
-        with open("account.txt", "r") as old:
-            raw = old.readlines()
-        if (raw[0][:3] != "uid") or (len(raw[0]) < 10):
-            print("account.txt 内容无效, 请手动修改内容")
-            sys_exit()
-        uid = (raw[0].split(":"))[1].strip()
-        psw = (raw[1].split(":"))[1].strip()
-
-    else:
-        print("未找到account.txt, 判断为首次运行, 请接下来依次输入学号密码")
-        uid = input("学号：")
-        psw = getpass("密码：")
-        with open("account.txt", "w") as new:
-            tmp = "uid:" + uid + "\npsw:" + psw +\
-                "\n\n\n以上两行冒号后分别写上学号密码，不要加空格/换行，谢谢\n\n请注意文件安全，不要放在明显位置\n\n可以从dailyFudan.exe创建快捷方式到桌面"
-            new.write(tmp)
-        print("账号已保存在目录下account.txt，请注意文件安全，不要放在明显位置\n\n建议拉个快捷方式到桌面")
-
-    return uid, psw
+        self.output('')
 
 
-if __name__ == '__main__':
-    uid, psw = get_account()
-    # print(uid, psw)
-    zlapp_login = 'https://uis.fudan.edu.cn/authserver/login?' \
-                  'service=https://zlapp.fudan.edu.cn/site/ncov/fudanDaily'
-    code_url = "https://zlapp.fudan.edu.cn/backend/default/code"
-    daily_fudan = Zlapp(uid, psw,
-                        url_login=zlapp_login, url_code=code_url)
-    daily_fudan.login()
+if __name__ == "__main__":
+    # os.environ["TZ"] = "Asia/Shanghai"
+    # time.tzset()
 
-    daily_fudan.check()
-    daily_fudan.checkin()
-    # 再检查一遍
-    daily_fudan.check()
-    daily_fudan.close(1)
+    log_path = "logs/"
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+
+    logging.basicConfig(level = logging.INFO)
+    log_file = "logs/%s.log" % time.strftime("%Y%m%d%H%M", time.localtime())
+    log_handler = logging.FileHandler(log_file, mode = "w")
+    log_formatter = logging.Formatter("%(asctime)s: %(message)s")
+    log_handler.setFormatter(log_formatter)
+    logger = logging.getLogger(__name__)
+    logger.addHandler(log_handler)
+
+    fudan_id, fudan_password, pushplus_token = get_account(output = logger.info)
+    login_url = "https://uis.fudan.edu.cn/authserver/login?service=https://zlapp.fudan.edu.cn/site/ncov/fudanDaily"
+
+    zlapp = Zlapp(fudan_id, fudan_password, pushplus_token,
+                  login_url = login_url, log_file = log_file, output = logger.info)
+
+    zlapp.login()
+    zlapp.check()
+    zlapp.submit()
+    zlapp.check()
+    zlapp.close()
